34a471d663d46682196ad21949378c830b14dcf7
Mei Sun <meysunlee@gmail.com>
2019-11-02 11:47:39 +0200

    submit part3.16

--- index.js
+++ index.js
@@ -30,15 +30,16 @@ app.get('/info', (request, response) => {
 
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    // if (person) {
-    //     response.json(person)
-    // } else {
-    //     response.status(404).end()
-    // }
-    person ? response.json(person) : response.status(404).end()
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person.toJSON())
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -54,11 +55,6 @@ app.post('/api/persons', (request, response) => {
     const person = request.body
     person.id = jsonPersonID
 
-    // const names = persons.map(person => person.name)
-    // if (names.includes(person.name)) {
-    //     return response.status(400).json({
-    //         error: 'name must be unique'
-    //     })
     if (person.name === null || person.number === null) {
         return response.status(400).json({
             error: 'name or number can not be empty'
@@ -73,20 +69,23 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
-
-
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind === 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
