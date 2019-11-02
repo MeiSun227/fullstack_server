88ab39e23fa9ce07331d85500335bbb67e682faa
Mei Sun <meysunlee@gmail.com>
2019-11-02 12:59:54 +0200

    submit part3.20

--- models/person.js
+++ models/person.js
@@ -9,12 +9,22 @@ mongoose.connect(url, { useNewUrlParser: true })
     .then(result => {
         console.log('connected to MongoDB')
     })
-    .catch((error) => { 
-        console.log('error connecting to MongoDB:', error.message) })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const personSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true},
-    number: {type: String, required: true},
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+    },
+    number: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
 })
 
 personSchema.set('toJSON', {
