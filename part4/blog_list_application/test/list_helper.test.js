40c5e5cd5efd2ed8a1937f7de52ad1c7df3abae6
Mei Sun <meysunlee@gmail.com>
2019-11-06 21:54:31 +0200

    submit 4.5

--- part4/blog_list_application/test/list_helper.test.js
+++ part4/blog_list_application/test/list_helper.test.js
@@ -2,7 +2,7 @@ const listHelper = require('../utils/list_helper')
 
 
 describe('total likes', () => {
-  const listWithOneBlog = [
+  const listWithManyBlogs = [
     {
       _id: '5a422aa71b54a676234d17f8',
       title: 'Go To Statement Considered Harmful',
@@ -10,17 +10,33 @@ describe('total likes', () => {
       url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
       likes: 5,
       __v: 0
+    },
+    {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12
     }
   ]
   test('dummy returns one', () => {
     const blogs = []
-  
+
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
   })
 
   test('when list has only one blog equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    expect(result).toBe(5)
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    expect(result).toBe(17)
+  })
+
+  test('when list has more than one blog ', () => {
+    const answer = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12
+    }
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    console.log(result)
+    expect(result).toEqual(answer)
   })
 })
--- part4/blog_list_application/utils/list_helper.js
+++ part4/blog_list_application/utils/list_helper.js
@@ -2,16 +2,23 @@ const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes =(blogs)=> {
+const totalLikes = (blogs) => {
   const reducer = (sum, blog) => {
     console.log(blog)
     return sum + blog.likes
   }
   return blogs.length === 0
-    ? 0 
-    : blogs.reduce(reducer, 0) 
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, blog) => max.likes > blog.likes
+    ? max : blog)
 }
 
 module.exports = {
-  dummy,totalLikes
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
