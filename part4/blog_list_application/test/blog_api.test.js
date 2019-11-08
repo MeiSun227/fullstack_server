2e8394eb463257ba205f934e6fa911597ae32f27
Mei Sun <meysunlee@gmail.com>
2019-11-08 20:22:58 +0200

    Submit part 4.10

--- part4/blog_list_application/test/blog_api.test.js
+++ part4/blog_list_application/test/blog_api.test.js
@@ -1,8 +1,34 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    likes: 12
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -14,7 +40,7 @@ test('blogs are returned as json', async () => {
 test('there is one blog', async () => {
   const response = await api.get('/api/blogs')
 
-expect(response.body.length).toBe(1)
+  expect(response.body.length).toBe(initialBlogs.length)
 })
 
 test('Verify blog with ID', async () => {
@@ -23,6 +49,27 @@ test('Verify blog with ID', async () => {
   expect(response.body[0].id).toBeDefined()
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    author: 'kissa ja koira',
+    likes: 100,
+    title: 'suomi elämä '
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const contents = response.body.map(r => r.author)
+
+  expect(response.body.length).toBe(initialBlogs.length + 1)
+  expect(contents).toContain(
+    'kissa ja koira'
+  )
+})
 
 afterAll(() => {
   mongoose.connection.close()
