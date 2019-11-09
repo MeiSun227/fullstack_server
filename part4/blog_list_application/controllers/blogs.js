70f03ba0c820278e2623af266664f0e4ed0e61d2
Mei Sun <meysunlee@gmail.com>
2019-11-09 18:00:35 +0200

    part 4.19

--- part4/blog_list_application/controllers/blogs.js
+++ part4/blog_list_application/controllers/blogs.js
@@ -1,25 +1,39 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   return response.json(blogs)
 })
-
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
-  const user = await User.findById(body.id)
+  const token = getTokenFrom(request)
 
-  const newBlog = new Blog({
-    title: body.title,
-    url: body.url,
-    author: body.author,
-    likes: body.likes,
-    user: body.id
-  })
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
 
-  try { 
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const newBlog = new Blog({
+      title: body.title,
+      url: body.url,
+      author: body.author,
+      likes: body.likes,
+      user: body.id
+    })
+
     const result = await newBlog.save()
     user.blogs = user.blogs.concat(result.id)
     await user.save()
